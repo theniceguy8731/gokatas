10a1e56f8d7da7cc152f7973362da130f9faf760
theniceguy8731 <kushagra22263@iiitd.ac.in>
2026-02-04 02:06:51 +0530

    minor update

---
+++ counter/cmd/counter.go
@@ -0,0 +1,27 @@
+// Package counter implements an integer counter that can only be incremented.
+//
+// Encapsulation (information hiding) is a key aspect of OOP. It prevents clients
+// from accessing variables or methods of an object. Benefits:
+//
+//  1. Clients need to inspect fewer statements.
+//  2. Clients don't depend on things that might change.
+//  3. Clients can't set object's variables arbitrarily.
+//
+// Adapted from the gopl.io ch. 6.6 Encapsulation.
+//
+// Level: beginner
+// Topics: oop, encapsulation
+package main
+
+import(
+	"fmt"
+	"github.com/jreisinger/gokatas/counter"
+)
+
+
+func main(){
+	c:=new(counter.Counter)
+	c.Increment()
+	fmt.Println(c.Get())
+	c.Reset()
+}
